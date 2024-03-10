83b025df0e0b52607e67b5853afae74fd8b352f8
Abel Beyene <abeyene@yorku.ca>
2024-03-09 23:51:13 -0500

    Fix axi4 bridge syntax errors

--- axi4_bridge/axi4_bridge.v
+++ axi4_bridge/axi4_bridge.v
@@ -14,7 +14,7 @@ module axi4lite_bridge
   input                                 clk,
   input                                 rstn,
   // Slave AXI4 Write Address Interface
-  input                                 s_axi4_aw_ready,
+  output logic                          s_axi4_aw_ready,
   input                                 s_axi4_aw_valid,
   input [axi4_id_size-1:0]              s_axi4_aw_id,
   input [axi4_addr_size-1:0]            s_axi4_aw_addr,
@@ -27,7 +27,7 @@ module axi4lite_bridge
   input [3:0]                           s_axi4_aw_qos,
   input [3:0]                           s_axi4_aw_region,
   // Master AXI4 Write Data Interface
-  output                                s_axi4_w_ready,
+  output logic                          s_axi4_w_ready,
   input                                 s_axi4_w_valid,
   input [axi4_id_size-1:0]              s_axi4_w_id,
   input [axi4_data_size-1:0]            s_axi4_w_data,
@@ -35,11 +35,11 @@ module axi4lite_bridge
   input                                 s_axi4_w_last,
   // Master AXI4 Write Response Interface
   input                                 s_axi4_b_ready,
-  output                                s_axi4_b_valid,
-  output [axi4_id_size-1:0]             s_axi4_b_id,
-  output [1:0]                          s_axi4_b_resp,
+  output logic                          s_axi4_b_valid,
+  output logic [axi4_id_size-1:0]       s_axi4_b_id,
+  output logic [1:0]                    s_axi4_b_resp,
   // Master AXI4 Read Address Interface
-  input                                 s_axi4_ar_ready,
+  output logic                          s_axi4_ar_ready,
   input                                 s_axi4_ar_valid,
   input [axi4_id_size-1:0]              s_axi4_ar_id,
   input [axi4_addr_size-1:0]            s_axi4_ar_addr,
@@ -53,34 +53,34 @@ module axi4lite_bridge
   input [3:0]                           s_axi4_ar_region,
   // Master AXI4 Read Data Interface
   input                                 s_axi4_r_ready,
-  output                                s_axi4_r_valid,
-  output [axi4_id_size-1:0]             s_axi4_r_id,
-  output [axi4_data_size-1:0]           s_axi4_r_data,
+  output logic                          s_axi4_r_valid,
+  output logic [axi4_id_size-1:0]       s_axi4_r_id,
+  output logic [axi4_data_size-1:0]     s_axi4_r_data,
   output                                s_axi4_r_last,
   // Master AXI4 Lite Write Address Interface
   input                                 m_axi4lite_aw_ready,
-  input                                 m_axi4lite_aw_valid,
-  input [axi4_addr_size-1:0]            m_axi4lite_aw_addr,
-  input [2:0]                           m_axi4lite_aw_prot,
+  output logic                          m_axi4lite_aw_valid,
+  output logic [axi4_addr_size-1:0]     m_axi4lite_aw_addr,
+  output logic [2:0]                    m_axi4lite_aw_prot,
   // Master AXI4 Lite Write Data Interface
-  output                                m_axi4lite_w_ready,
-  input                                 m_axi4lite_w_valid,
-  input [axi4_data_size-1:0]            m_axi4lite_w_data,
-  input [(axi4_data_size >> 3):0]       m_axi4lite_w_strb,
+  input                                 m_axi4lite_w_ready,
+  output logic                          m_axi4lite_w_valid,
+  output logic [axi4_data_size-1:0]     m_axi4lite_w_data,
+  output logic [(axi4_data_size>>3):0]  m_axi4lite_w_strb,
   // Master AXI4 Write Response Interface
-  input                                 m_axi4lite_b_ready,
-  output                                m_axi4lite_b_valid,
-  output [1:0]                          m_axi4lite_b_resp,
+  output logic                          m_axi4lite_b_ready,
+  input                                 m_axi4lite_b_valid,
+  input [1:0]                           m_axi4lite_b_resp,
   // Master AXI4 Read Address Interface
   input                                 m_axi4lite_ar_ready,
-  input                                 m_axi4lite_ar_valid,
-  input [axi4_addr_size-1:0]            m_axi4lite_ar_addr,
-  input [2:0]                           m_axi4lite_ar_prot,
+  output logic                          m_axi4lite_ar_valid,
+  output logic [axi4_addr_size-1:0]     m_axi4lite_ar_addr,
+  output logic [2:0]                    m_axi4lite_ar_prot,
   // Master AXI4 Read Data Interface
-  input                                 m_axi4lite_r_ready,
-  output                                m_axi4lite_r_valid,
-  output [axi4_data_size-1:0]           m_axi4lite_r_data,
-  output [1:0]                          m_axi4lite_r_resp
+  output logic                          m_axi4lite_r_ready,
+  input                                 m_axi4lite_r_valid,
+  input [axi4_data_size-1:0]            m_axi4lite_r_data,
+  input [1:0]                           m_axi4lite_r_resp
   );
 
   typedef struct packed {
@@ -118,10 +118,14 @@ module axi4lite_bridge
   axi4_w_pkt_t axi4_w_pkt; 
   axi4_r_pkt_t axi4_r_pkt;
 
+  wire s_axi4_aw_fifo_data_in_valid, s_axi4_aw_fifo_data_in_ready; 
+  wire [$bits(axi4_aw_pkt)-1:0] s_axi4_aw_fifo_data_in;
+
+  wire s_axi4_w_done_r;
   /************************* BEGIN: Slave AXI4 AW Logic ***********************/
 
   typedef enum logic [3:0] {S_AXI4_AW_STATE_IDLE, S_AXI4_AW_STATE_CHECK, S_AXI4_AW_STATE_CONVERT, S_AXI4_AW_STATE_STORE, S_AXI4_AW_STATE_WAIT} s_axi4_aw_state_t;
-  s_axi4_aw_state_t s_axi4_aw_state, s_axi4_aw_state_n;
+  s_axi4_aw_state_t s_axi4_aw_state, s_axi4_aw_state_n, s_axi4_aw_state_p;
 
   reg [axi4_addr_size-1:0] s_axi4_aw_aligned_addr_r;
   reg [axi4_addr_size-1:0] s_axi4_aw_wrap_boundary_r;
@@ -150,7 +154,7 @@ module axi4lite_bridge
           s_axi4_aw_fifo_data_in_valid = 1'b0;
           if (s_axi4_aw_valid)
           begin
-            s_axi4_aw_state_n = S_AXI4_AW_STATE_CHECK
+            s_axi4_aw_state_n = S_AXI4_AW_STATE_CHECK;
           end
         end
       S_AXI4_AW_STATE_CHECK :
@@ -160,26 +164,27 @@ module axi4lite_bridge
         case (axi4_aw_pkt.len)
           2'b00 : 
             begin
-              s_axi4_aw_state_n = S_AXI4_AW_STATE_STORE
+              s_axi4_aw_state_n = S_AXI4_AW_STATE_STORE;
             end
           2'b01 : 
             begin
-              s_axi4_aw_state_n = S_AXI4_AW_STATE_CONVERT
+              s_axi4_aw_state_n = S_AXI4_AW_STATE_CONVERT;
             end
           2'b10 : 
             begin
-              s_axi4_aw_state_n = S_AXI4_AW_STATE_CONVERT
+              s_axi4_aw_state_n = S_AXI4_AW_STATE_CONVERT;
             end
           default : 
             begin
-              s_axi4_aw_state_n = S_AXI4_AW_STATE_IDLE
+              s_axi4_aw_state_n = S_AXI4_AW_STATE_IDLE;
             end
+          endcase
         end
       S_AXI4_AW_STATE_CONVERT :
         begin
           s_axi4_aw_ready = 1'b0;
           s_axi4_aw_fifo_data_in_valid = 1'b0;
-          s_axi4_aw_state_n = S_AXI4_AW_STATE_STORE
+          s_axi4_aw_state_n = S_AXI4_AW_STATE_STORE;
         end
       S_AXI4_AW_STATE_STORE :
         begin
@@ -207,10 +212,12 @@ module axi4lite_bridge
     if (~rstn)
     begin
       s_axi4_aw_state <= S_AXI4_AW_STATE_IDLE;
+      s_axi4_aw_state_p <= S_AXI4_AW_STATE_IDLE;
     end
     else
     begin
-      s_axi4_aw_state <= s_axi4_aw_state_n;
+      s_axi4_aw_state   <= s_axi4_aw_state_n;
+      s_axi4_aw_state_p <= s_axi4_aw_state;
     end
   end
 
@@ -218,7 +225,7 @@ module axi4lite_bridge
   begin
     if (rstn)
     begin
-      if (state == S_AXI4_AW_STATE_CHECK & state_p = S_AXI4_AW_STATE_IDLE)
+      if (s_axi4_aw_state == S_AXI4_AW_STATE_CHECK & s_axi4_aw_state_p == S_AXI4_AW_STATE_IDLE)
       begin
         axi4_aw_pkt.id <= s_axi4_aw_id;
         axi4_aw_pkt.addr <= s_axi4_aw_addr;
@@ -232,7 +239,7 @@ module axi4lite_bridge
         axi4_aw_pkt.lock <= s_axi4_aw_lock;
 
         s_axi4_aw_aligned_addr_r <= s_axi4_aw_addr & ({axi4_addr_size{1'b1}} << s_axi4_aw_size);
-        s_axi4_aw_wrap_boundary_r <= s_axi4_aw_addr & ({axi4_addr_size{1'b1}} << (s_axi4_aw_len << (1 << s_axi4_aw_size));
+        s_axi4_aw_wrap_boundary_r <= s_axi4_aw_addr & ({axi4_addr_size{1'b1}} << (s_axi4_aw_len << (1 << s_axi4_aw_size)));
       end
     end
   end
@@ -241,15 +248,15 @@ module axi4lite_bridge
   begin
     if (rstn)
     begin
-      if (state == S_AXI4_AW_STATE_CHECK & state_n == S_AXI4_AW_STATE_CONVERT)
+      if (s_axi4_aw_state == S_AXI4_AW_STATE_CHECK & s_axi4_aw_state_n == S_AXI4_AW_STATE_CONVERT)
       begin
         s_axi4_aw_burst_count_r <= 3'b0;
         s_axi4_aw_addr_offset_r <= 1 << axi4_aw_pkt.size;
         s_axi4_aw_other_addr_r  <= axi4_aw_pkt.addr;
       end
-      if (state == S_AXI4_AW_STATE_CONVERT && state_n == S_AXI4_AW_STATE_STORE)
+      if (s_axi4_aw_state == S_AXI4_AW_STATE_CONVERT && s_axi4_aw_state_n == S_AXI4_AW_STATE_STORE)
       begin
-        if (axi4_aw_burst_count != 3'b0)
+        if (s_axi4_aw_burst_count_r != 3'b0)
         begin
           case (axi4_aw_pkt.burst)
             2'b01 : axi4_aw_pkt.addr <= s_axi4_aw_aligned_addr_r + s_axi4_aw_addr_offset_r;
@@ -257,16 +264,16 @@ module axi4lite_bridge
               begin
                   if (axi4_aw_pkt.addr < s_axi4_aw_wrap_boundary_r)
                     axi4_aw_pkt.addr <= s_axi4_aw_aligned_addr_r + s_axi4_aw_addr_offset_r;
-                  else if (axi4_aw_pkt.addr == s_axi4_aw_wrap_boundary_r + (s_axi4_aw_len << (1 << s_axi4_aw_size)));
+                  else if (axi4_aw_pkt.addr == s_axi4_aw_wrap_boundary_r + (s_axi4_aw_len << (1 << s_axi4_aw_size)))
                     axi4_aw_pkt.addr <= s_axi4_aw_wrap_boundary_r;
                   else
-                    axi4_aw_pkt.addr <= s_axi4_aw_other_addr_r - (s_axi4_aw_len << (1 << s_axi4_aw_size)));
+                    axi4_aw_pkt.addr <= s_axi4_aw_other_addr_r - (s_axi4_aw_len << (1 << s_axi4_aw_size));
               end
             default : 
                     axi4_aw_pkt.addr <= {axi4_addr_size{1'b0}};
           endcase
         end
-        s_axi4_aw_burst_count_r <= s_axi4_aw_burst_count + 8'b1; 
+        s_axi4_aw_burst_count_r <= s_axi4_aw_burst_count_r + 8'b1; 
         s_axi4_aw_addr_offset_r <= s_axi4_aw_addr_offset_r + (1 << axi4_aw_pkt.size);
         s_axi4_aw_other_addr_r <= s_axi4_aw_other_addr_r + (1 << axi4_aw_pkt.size);
       end
@@ -276,8 +283,6 @@ module axi4lite_bridge
 
   /************************* END: Slave AXI4 AW Logic ***********************/
 
-  wire s_axi4_aw_fifo_data_in_valid, s_axi4_aw_fifo_data_in_ready; 
-  wire [$bits(axi4_aw_pkt)-1:0] s_axi4_aw_fifo_data_in;
 
   assign s_axi4_aw_fifo_data_in = {axi4_aw_pkt.region, axi4_aw_pkt.qos, axi4_aw_pkt.prot, axi4_aw_pkt.lock, axi4_aw_pkt.cache, axi4_aw_pkt.len, axi4_aw_pkt.size,  axi4_aw_pkt.burst, axi4_aw_pkt.addr, axi4_aw_pkt.id};
 
